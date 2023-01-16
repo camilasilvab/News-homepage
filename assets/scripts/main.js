d85a571e7087f05dce3d50c05e6d5cc5d1fc45ff
kyambvthi <kyambvthi@gmail.com>
2023-01-16 12:42:09 +0300

    moved the main.js file into the assets/scripts file.

---
+++ assets/scripts/main.js
@@ -0,0 +1,11 @@
+const button = document.querySelector('.burger-button')
+const nav = document.querySelector('.nav')
+const close = document.querySelector('.close-menu')
+
+button.addEventListener('click',()=>{
+    nav.classList.toggle('active')
+})
+
+close.addEventListener('click',()=>{
+    nav.classList.toggle('inactive')
+})
