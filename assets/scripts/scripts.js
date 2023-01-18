8dece2fefe4610760a5a62a265642309753249f4
kyambvthi <kk.mbuthi@gmail.com>
2023-01-18 20:26:54 +0300

    added scripts.js

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,15 @@
+function readFromSource() {
+    // get story data from source.
+}
+
+function buildArticleComponent() {
+    // build an article component.
+}
+
+function buildTimelineComponent() {
+    // build a timeline component.
+}
+
+function populateTimeline() {
+    // populate the timeline with articles.
+}
